cef5fb34c5159e48c7eebe4525ef2e75ce826e9d
Stephen Hillenbrand <Stephen.Hillenbrand@tafesa.edu.au>
2023-09-25 11:50:15 +0930

    Add Database Scripts create.sql, insert.sql

---
+++ Database Scripts/create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE subject (
+  subject_id INT NOT NULL AUTO_INCREMENT,
+  subject_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (subject_id)
+);
+
+CREATE TABLE lecturer (
+  lecturer_id INT NOT NULL AUTO_INCREMENT,
+  lecturer_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (lecturer_id)
+);
+
+CREATE TABLE room (
+  room_id INT NOT NULL AUTO_INCREMENT,
+  room_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (room_id)
+);
+
+CREATE TABLE student (
+  student_id INT NOT NULL AUTO_INCREMENT,
+  student_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (student_id)
+);
---
+++ Database Scripts/insert.sql
@@ -0,0 +1,47 @@
+INSERT INTO subject (subject_name) VALUES
+('Underwater Basketweaving'),
+('Underwater Basketweaving History'),
+('Underwater Basketweaving Techniques'),
+('Underwater Basketweaving Design'),
+('Underwater Basketweaving Business'),
+('Underwater Basketweaving Photography'),
+('Underwater Basketweaving Marketing'),
+('Underwater Basketweaving Sales'),
+('Underwater Basketweaving Customer Service'),
+('Underwater Basketweaving Employment Opportunities');
+
+INSERT INTO lecturer (lecturer_name) VALUES
+('Alice Smith'),
+('Bob Jones'),
+('Charlie Brown'),
+('Donna Johnson'),
+('Eric Williams'),
+('Fiona Taylor'),
+('George Johnson'),
+('Helen Williams'),
+('Ian Taylor'),
+('Jack Johnson');
+
+INSERT INTO room (room_name) VALUES
+('Room 101'),
+('Room 102'),
+('Room 103'),
+('Room 104'),
+('Room 105'),
+('Room 106'),
+('Room 107'),
+('Room 108'),
+('Room 109'),
+('Room 110');
+
+INSERT INTO student (student_name) VALUES
+('David Williams'),
+('Emily Taylor'),
+('Frank Johnson'),
+('Grace Miller'),
+('Henry Brown'),
+('Isabella Smith'),
+('James Jones'),
+('Katherine Williams'),
+('Liam Taylor'),
+('Michael Johnson');
