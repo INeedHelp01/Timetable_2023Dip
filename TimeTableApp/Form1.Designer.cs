a32ff3f8606367324241a0da3bf2777c9494e76f
Andre <44639490+INeedHelp01@users.noreply.github.com>
2023-09-27 13:43:17 +0930

    Added Database connectivity

--- TimeTableApp/Form1.Designer.cs
+++ TimeTableApp/Form1.Designer.cs
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Slot11 = new System.Windows.Forms.RichTextBox();
+            this.Slot21 = new System.Windows.Forms.RichTextBox();
+            this.Slot31 = new System.Windows.Forms.RichTextBox();
+            this.Slot12 = new System.Windows.Forms.RichTextBox();
+            this.Slot13 = new System.Windows.Forms.RichTextBox();
+            this.Slot14 = new System.Windows.Forms.RichTextBox();
+            this.Slot22 = new System.Windows.Forms.RichTextBox();
+            this.Slot32 = new System.Windows.Forms.RichTextBox();
+            this.Slot23 = new System.Windows.Forms.RichTextBox();
+            this.Slot24 = new System.Windows.Forms.RichTextBox();
+            this.Slot33 = new System.Windows.Forms.RichTextBox();
+            this.Slot34 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Slot11
+            // 
+            this.Slot11.Location = new System.Drawing.Point(29, 24);
+            this.Slot11.Name = "Slot11";
+            this.Slot11.Size = new System.Drawing.Size(100, 96);
+            this.Slot11.TabIndex = 0;
+            this.Slot11.Text = "";
+            // 
+            // Slot21
+            // 
+            this.Slot21.Location = new System.Drawing.Point(29, 127);
+            this.Slot21.Name = "Slot21";
+            this.Slot21.Size = new System.Drawing.Size(100, 96);
+            this.Slot21.TabIndex = 1;
+            this.Slot21.Text = "";
+            // 
+            // Slot31
+            // 
+            this.Slot31.Location = new System.Drawing.Point(29, 230);
+            this.Slot31.Name = "Slot31";
+            this.Slot31.Size = new System.Drawing.Size(100, 96);
+            this.Slot31.TabIndex = 2;
+            this.Slot31.Text = "";
+            // 
+            // Slot12
+            // 
+            this.Slot12.Location = new System.Drawing.Point(136, 24);
+            this.Slot12.Name = "Slot12";
+            this.Slot12.Size = new System.Drawing.Size(100, 96);
+            this.Slot12.TabIndex = 3;
+            this.Slot12.Text = "";
+            // 
+            // Slot13
+            // 
+            this.Slot13.Location = new System.Drawing.Point(242, 25);
+            this.Slot13.Name = "Slot13";
+            this.Slot13.Size = new System.Drawing.Size(100, 96);
+            this.Slot13.TabIndex = 4;
+            this.Slot13.Text = "";
+            // 
+            // Slot14
+            // 
+            this.Slot14.Location = new System.Drawing.Point(350, 24);
+            this.Slot14.Name = "Slot14";
+            this.Slot14.Size = new System.Drawing.Size(100, 96);
+            this.Slot14.TabIndex = 5;
+            this.Slot14.Text = "";
+            // 
+            // Slot22
+            // 
+            this.Slot22.Location = new System.Drawing.Point(136, 127);
+            this.Slot22.Name = "Slot22";
+            this.Slot22.Size = new System.Drawing.Size(100, 96);
+            this.Slot22.TabIndex = 6;
+            this.Slot22.Text = "";
+            // 
+            // Slot32
+            // 
+            this.Slot32.Location = new System.Drawing.Point(136, 230);
+            this.Slot32.Name = "Slot32";
+            this.Slot32.Size = new System.Drawing.Size(100, 96);
+            this.Slot32.TabIndex = 7;
+            this.Slot32.Text = "";
+            // 
+            // Slot23
+            // 
+            this.Slot23.Location = new System.Drawing.Point(243, 127);
+            this.Slot23.Name = "Slot23";
+            this.Slot23.Size = new System.Drawing.Size(100, 96);
+            this.Slot23.TabIndex = 8;
+            this.Slot23.Text = "";
+            // 
+            // Slot24
+            // 
+            this.Slot24.Location = new System.Drawing.Point(350, 127);
+            this.Slot24.Name = "Slot24";
+            this.Slot24.Size = new System.Drawing.Size(100, 96);
+            this.Slot24.TabIndex = 9;
+            this.Slot24.Text = "";
+            // 
+            // Slot33
+            // 
+            this.Slot33.Location = new System.Drawing.Point(243, 230);
+            this.Slot33.Name = "Slot33";
+            this.Slot33.Size = new System.Drawing.Size(100, 96);
+            this.Slot33.TabIndex = 10;
+            this.Slot33.Text = "";
+            // 
+            // Slot34
+            // 
+            this.Slot34.Location = new System.Drawing.Point(350, 230);
+            this.Slot34.Name = "Slot34";
+            this.Slot34.Size = new System.Drawing.Size(100, 96);
+            this.Slot34.TabIndex = 11;
+            this.Slot34.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Slot34);
+            this.Controls.Add(this.Slot33);
+            this.Controls.Add(this.Slot24);
+            this.Controls.Add(this.Slot23);
+            this.Controls.Add(this.Slot32);
+            this.Controls.Add(this.Slot22);
+            this.Controls.Add(this.Slot14);
+            this.Controls.Add(this.Slot13);
+            this.Controls.Add(this.Slot12);
+            this.Controls.Add(this.Slot31);
+            this.Controls.Add(this.Slot21);
+            this.Controls.Add(this.Slot11);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox Slot11;
+        private System.Windows.Forms.RichTextBox Slot21;
+        private System.Windows.Forms.RichTextBox Slot31;
+        private System.Windows.Forms.RichTextBox Slot12;
+        private System.Windows.Forms.RichTextBox Slot13;
+        private System.Windows.Forms.RichTextBox Slot14;
+        private System.Windows.Forms.RichTextBox Slot22;
+        private System.Windows.Forms.RichTextBox Slot32;
+        private System.Windows.Forms.RichTextBox Slot23;
+        private System.Windows.Forms.RichTextBox Slot24;
+        private System.Windows.Forms.RichTextBox Slot33;
+        private System.Windows.Forms.RichTextBox Slot34;
     }
 }
 
--- TimeTableApp/Form1.cs
+++ TimeTableApp/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using MySqlConnector;
 
 namespace TimeTableApp
 {
@@ -16,5 +19,38 @@ namespace TimeTableApp
         {
             InitializeComponent();
         }
+
+        string myConnectionString = "server=127.0.0.1;uid=root;" +
+    "pwd=r3wG!Y$!jPoe57;database=TimeTable";
+        MySql.Data.MySqlClient.MySqlConnection conn;
+
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                Console.WriteLine("connected");
+                MySql.Data.MySqlClient.MySqlCommand command = new 
+                    MySql.Data.MySqlClient.MySqlCommand("SELECT * FROM lecturer", conn);
+                MySql.Data.MySqlClient.MySqlDataReader reader = command.ExecuteReader();
+
+                List<string> teachers = new List<string>();
+                while (reader.Read())
+                {
+                    teachers.Add(reader["lecturer_id"].ToString());
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Console.WriteLine("oops");
+            }
+
+        }
+
     }
 }
